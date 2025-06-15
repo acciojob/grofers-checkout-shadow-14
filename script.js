1c1e9accdb2920a8bb1bff7688fca74a300623d3
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-15 09:29:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
- let prices = document.querySelectorAll('.price');
+ let prices = document.querySelectorAll('.prices');
 	let sum =0;
 prices.forEach((ele)=>{ const value = parseFloat(ele.textContent);
   if (!isNaN(value)) {
@@ -13,7 +13,7 @@ prices.forEach((ele)=>{ const value = parseFloat(ele.textContent);
 
 const totalRow = document.createElement('tr');
 const cell1 = document.createElement('td');
-cell1.textContent = 'Total';
+cell1.textContent = 'total value';
 
 const cell2 = document.createElement('td');
 cell2.textContent = sum;
@@ -21,6 +21,7 @@ cell2.textContent = sum;
 // Append cells to row
 totalRow.appendChild(cell1);
 totalRow.appendChild(cell2);
+totalRow.id ="ans";
 
 // Append row to the table
 const table = document.querySelector('table');
